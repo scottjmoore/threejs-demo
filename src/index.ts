374f96c955289fc2f8ca3c4625b38065eb4f4ef3
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-23 21:28:59 +0100

    Add some more objects

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,93 @@
-import { Version } from './version';
-let THREE = require('three');
-let threejsdiv = document.getElementById('ThreeJS');
+import THREE = require("three");
 
-threejsdiv.innerHTML += `three.js version ${THREE.REVISION}<br>thteejs-typescript-webpack version ${Version}`;
+const canvas: any = document.querySelector("#threejscanvas");
+
+function main() {
+  const canvas: any = document.querySelector("#threejscanvas");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const fov = 75;
+  const aspect = canvas.clientWidth / canvas.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 3;
+
+  const scene = new THREE.Scene();
+
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
+  function addMesh(geometry, parent, color, x, y, z) {
+    const material = new THREE.MeshPhongMaterial({ color });
+
+    const mesh = new THREE.Mesh(geometry, material);
+
+    if (parent != null) {
+      parent.add(mesh);
+    } else {
+      scene.add(mesh);
+    }
+
+    mesh.position.x = x;
+    mesh.position.y = y;
+    mesh.position.z = z;
+
+    return mesh;
+  }
+
+  let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  let sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+  let cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+
+  var cubeMesh: THREE.Mesh, cylinderMesh: THREE.Mesh;
+
+  const meshes = [
+    (cubeMesh = addMesh(cubeGeometry, null, 0xff0000, 0, 0, 0)),
+    (cylinderMesh = addMesh(cylinderGeometry, cubeMesh, 0x00ff00, -2, 0, 0)),
+    addMesh(sphereGeometry, cylinderMesh, 0x0000ff, 2, 0, 0)
+  ];
+
+  function render(time) {
+    time *= 0.001; // convert time to seconds
+
+    meshes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function onWindowResize() {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+
+    console.log(`${width},${height},${needResize}`);
+
+    if (needResize) {
+      renderer.setSize(width, height, false);
+
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+  }
+
+  window.addEventListener("resize", onWindowResize, false);
+  requestAnimationFrame(render);
+}
+
+main();
